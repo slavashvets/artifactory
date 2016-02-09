fa0039ccbc3c18864d06a4ead69192426029602c
elig <elig@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2016-02-09 08:26:46 +0000

    Tagging version 4.4.3

---
+++ public/tags/4.4.3/trunk/web/angular-web/src/main/webapp/app/constants/user_actions.constants.js
@@ -0,0 +1,7 @@
+export default {
+    'RevokeApiKey': {title: 'Revoke Api Key', icon: 'icon-clear2'},
+    'UnlockUser': {title: 'Unlock User', icon: 'icon-unlock'},
+    'ExpirePassword': {title: 'Expire Password', icon: 'icon-clear2'},
+    'UnexpirePassword': {title: 'Unexpire Password', icon: 'icon-go'},
+    'DeleteUser': {title: 'Delete User', icon: 'icon-clear'}
+};
