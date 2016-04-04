1002069f1bdb3c8a8d4fb816207a0ba81d5069b9
elig <elig@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2016-04-04 11:42:25 +0000

    Updating trunk to version 4.7.1

---
+++ public/trunk/binary-store/src/main/java/org/artifactory/storage/binstore/binary/providers/StorageUnit.java
@@ -0,0 +1,10 @@
+package org.artifactory.storage.binstore.binary.providers;
+
+/**
+ * @author Gidi Shabat
+ */
+public interface StorageUnit {
+    static String toReadableString(long currentSize) {
+        return null;
+    }
+}
