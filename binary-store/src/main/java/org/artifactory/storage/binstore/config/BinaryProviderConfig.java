7110e1ea3bd7d329af5403c8ef3b8abca231ea0f
elig <elig@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2016-04-17 19:27:38 +0000

    Updating trunk to version 4.7.3

---
+++ binary-store/src/main/java/org/artifactory/storage/binstore/config/BinaryProviderConfig.java
@@ -0,0 +1,47 @@
+package org.artifactory.storage.binstore.config;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+/**
+ * @author Gidi Shabat
+ */
+public class BinaryProviderConfig {
+    private Map<String, String> params = Maps.newHashMap();
+    private Map<String, String> properties = Maps.newHashMap();
+    /**
+     *
+     */
+    private String binaryStoreXmlPath;
+
+    public String getBinaryStoreXmlPath() {
+        return binaryStoreXmlPath;
+    }
+
+    public void setBinaryStoreXmlPath(String binaryStoreXmlPath) {
+        this.binaryStoreXmlPath = binaryStoreXmlPath;
+    }
+
+    public void addParam(String key, String value) {
+        params.put(key, value);
+    }
+
+    public void addProperty(String key, String value) {
+        properties.put(key,value);
+    }
+
+    public String getParam(String key) {
+        return params.get(key);
+    }
+
+    public Map<String, String> getProperties() {
+        Map<String, String> result = Maps.newHashMap();
+        properties.keySet().stream().forEach(key -> {
+            result.put(key, properties.get(key));
+        });
+        return result;
+    }
+
+
+}
